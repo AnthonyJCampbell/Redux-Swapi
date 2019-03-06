ae7f657035ad57e1e2d22ea32fc1cb78bbedf888
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-03-06 14:09:34 +0100

    Added Actions, Action Creators and Reducers

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,35 @@
-import /* we need our action types here*/ "../actions";
+import * as types from "../actions";
 const initialState = {
-  characters: []
+  characters: [],
+  fetching: false,
+  error: null,
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
+  console.log('reducer', action)
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case types.FETCHING:
+      return {
+        ...state,
+        isfetching: true,
+        error: null,
+      };
+    case types.SUCCESS:
+      return {
+        ...state,
+        characters: action.payload,
+        fetching: false,
+        error: null,
+      };
+    case types.FAILURE:
+      return {
+        ...state,
+        fetching: false,
+        error: action.payload,
+      };
     default:
       return state;
   }
